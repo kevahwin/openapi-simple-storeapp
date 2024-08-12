47abbbcbfe56f055f0b1258253a54bece86bea88
Kevin Ahwin <kevahwin@gmail.com>
2024-08-12 10:20:26 +0100

    openapi.yaml: remove prop info.license.identifier

---
+++ src/test/java/com/kevahwin/openapi_simple_storeapp/OpenapiSimpleStoreappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevahwin.openapi_simple_storeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSimpleStoreappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
