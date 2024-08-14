0c097c3de7651d80d093d7cdfd4c3037983e92ab
Kevin Ahwin <kevahwin@gmail.com>
2024-08-14 11:12:53 +0100

    Add repository and controller for customer entity

--- src/test/java/com/kevahwin/openapi_simple_storeapp/OpenapiSimpleStoreappApplicationTests.java
+++ src/test/java/com/kevahwin/openapi_simple_storeapp/OpenapiSimpleStoreappApplicationTests.java
@@ -1,13 +1,25 @@
 package com.kevahwin.openapi_simple_storeapp;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import com.kevahwin.openapi_simple_storeapp.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OpenapiSimpleStoreappApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testDataLoad(){
+		assertThat(customerRepository.count()).isGreaterThan(0L);
 	}
 
 }
